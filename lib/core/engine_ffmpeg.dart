39cae935427e3a885b3a1a7bdd53b128017713f7
ali12hhh-oss <ali12.hhh@gmail.com>
2026-07-03 00:29:33 +0300

    Update engine_ffmpeg.dart

--- lib/core/engine_ffmpeg.dart
+++ lib/core/engine_ffmpeg.dart
@@ -1,6 +1,6 @@
 ﻿// ملاحظة: ستحتاج لإضافة مكتبة flutter_ffmpeg أو ffmpeg_kit_flutter في pubspec.yaml
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit_flutter_full_lts/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_full_lts/ffmpeg_kit.dart';
 
 class EngineFFmpeg {
   
