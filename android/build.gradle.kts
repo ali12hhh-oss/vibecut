77806c8bfa12bf78ce37b4c78651f827e695b1eb
ali12hhh-oss <ali12.hhh@gmail.com>
2026-07-03 00:47:01 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,12 @@
+// الملف الجاهز: android/build.gradle.kts
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+        // مستودع JitPack للمكتبات المخصصة
         maven { url = uri("https://jitpack.io") }
+        // مستودع Maven الرئيسي لضمان العثور على حزم FFmpeg
         maven { url = uri("https://repo.maven.apache.org/maven2") }
     }
 }
@@ -17,6 +21,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
