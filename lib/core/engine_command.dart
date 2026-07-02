392c1ad7be90cf882820838cf2ea92bc0e743e1a
ali12hhh-oss <ali12.hhh@gmail.com>
2026-07-03 00:28:29 +0300

    Update engine_command.dart

--- lib/core/engine_command.dart
+++ lib/core/engine_command.dart
@@ -1,5 +1,5 @@
-﻿import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/return_code.dart';
+﻿import 'package:ffmpeg_kit_flutter_full_lts/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_full_lts/ffmpeg_kit.dart';
 
 class EngineCommand {
   
